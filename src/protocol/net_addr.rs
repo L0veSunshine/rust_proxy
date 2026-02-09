e88079a97e24aa721b40728c1b2dcff9c2848866
L0veSunshine <xuan199651@gmail.com>
2026-02-09 16:01:32 +0800

    style: align comments for better readability in `net_addr.rs`

--- src/protocol/net_addr.rs
+++ src/protocol/net_addr.rs
@@ -172,9 +172,9 @@ impl NetAddr {
 
     pub fn bytes_len(&self) -> usize {
         match self {
-            NetAddr::V4(..) => 7,                           // 1 + 4 + 2
+            NetAddr::V4(..) => 7,                            // 1 + 4 + 2
             NetAddr::Domain(domain, ..) => 4 + domain.len(), // 1 + 1 + len + 2
-            NetAddr::V6(..) => 19,                          // 1 + 16 + 2
+            NetAddr::V6(..) => 19,                           // 1 + 16 + 2
         }
     }
 
